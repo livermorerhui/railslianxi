8e4a4c95638ab7a0ece8f0b05fadc18bb816acf7
xuhangwu <xuhangwu5244@gmail.com>
2017-10-24 17:18:00 +0800

    使用helper和partial优化代码，增加先加入后发表文章限制

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,15 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_id_and_check_permission, only: [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+
+    if !current_user.is_member_of?(@group)
+      flash[:warning] = "加入后才能发表文章"
+      redirect_to group_path(@group)
+    end
   end
 
   def create
@@ -18,7 +24,34 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @post.group = @group
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path, alert: "删除成功"
+  end
+
   private
+
+  def find_id_and_check_permission
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to account_posts_path, alert: "You have no permission/非创建者，无权限操作"
+    end
+
+  end
 
   def post_params
     params.require(:post).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,9 @@ Rails.application.routes.draw do
     resources :posts
   end
 
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
 
 end
